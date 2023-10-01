a2fcb58a5c16d234f9621ab7f9a0f16010cfda09
vyshnavisaai <vyshnavihrishikeshpv@gmail.com>
2023-10-01 22:41:16 +0530

    Inline styling in react

---
+++ Inlinestyling.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+function Inlinestyling() {
+  return (
+    <div>
+        <h1 style={{color:"red"}}>WELCOME TO MY PAGE</h1>
+        <p style={{backgroundColor:"yellow"}}>Styling inline</p>
+        </div>
+  )
+}
+
+export default Inlinestyling
