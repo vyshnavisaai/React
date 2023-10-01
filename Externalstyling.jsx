564a721236356d60f7d89da21536b1cf0ada77e6
vyshnavisaai <vyshnavihrishikeshpv@gmail.com>
2023-10-01 22:37:41 +0530

    External styling in react

---
+++ Externalstyling.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import './Externalstyling.css'
+
+function Externalstyling() {
+  return (
+    <div>
+        <h2>Styling</h2>
+        <p>External styling</p>
+    </div>
+  )
+}
+
+export default Externalstyling
