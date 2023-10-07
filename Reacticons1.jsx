905b5cd68ad4db7fe66d51679636900e24afd3fa
vyshnavisaai <vyshnavihrishikeshpv@gmail.com>
2023-10-07 12:27:25 +0530

    Using react icons in react

---
+++ Reacticons1.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+import { HiAcademicCap } from "react-icons/hi"
+import { BsPerson, BsPersonVcard } from "react-icons/bs"
+
+function Reacticons1() {
+  return (
+    <div>
+        <h1><HiAcademicCap/></h1>
+        <h1><BsPerson/></h1>
+        <h1><BsPersonVcard/></h1>
+    </div>
+  )
+}
+
+export default Reacticons1
