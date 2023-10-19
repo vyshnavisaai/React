ae2a0681bf86fed8c257ccdfee1b38e83643ed99
vyshnavisaai <vyshnavihrishikeshpv@gmail.com>
2023-10-19 12:47:56 +0530

    props in react

---
+++ PropsChild1.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import { First } from 'react-bootstrap/esm/PageItem'
+
+function PropsChild1({first}) {
+ 
+  return (
+    <div>
+        <h1>{first}</h1>
+    </div>
+  )
+}
+
+export default PropsChild1
