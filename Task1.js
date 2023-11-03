ce81aab1c9efc86976f4f764197c049ca58f160c
vyshnavisaai <vyshnavihrishikeshpv@gmail.com>
2023-11-03 21:38:29 +0530

    Task :useContext,useNavigate,useState (js)

---
+++ Task1.js
@@ -0,0 +1,22 @@
+const fruits = [
+  {
+    fruit: "Apple",
+    Image:
+      "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRyCoVvFRMWu7ZUgzjpXMoldZgfY0_fqKspQA&usqp=CAU",
+    def: "An apple is a round, edible fruit produced by an apple tree. Apple trees are cultivated worldwide and are the most widely grown species.",
+  },
+  {
+    fruit: "Mango",
+    Image:
+      "https://www.verywellhealth.com/thmb/hXMvqirIZ98Gn6w5bkhO4UkkXRE=/1500x0/filters:no_upscale():max_bytes(150000):strip_icc()/GettyImages-1392614128-b6ff76b34a7f429995ed571015d1a680.jpg",
+    def: "A mango is an edible stone fruit produced by the tropical tree Mangifera indica. It is believed to have originated in southern Asia.",
+  },
+  {
+    fruit: "Pomagranate",
+    Image:
+      "https://images.healthshots.com/healthshots/en/uploads/2021/09/27184641/pomegranate-1600x900.jpg",
+    def: "The pomegranate (Punica granatum) is a fruit-bearing deciduous shrub in the family Lythraceae, subfamily Punicoideae.",
+  },
+];
+
+export default fruits;
