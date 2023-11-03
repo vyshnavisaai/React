84a8ea0431db1f64bd21f2485d501bdfa2a918e2
vyshnavisaai <vyshnavihrishikeshpv@gmail.com>
2023-11-03 20:41:40 +0530

    useContext - method 2

---
+++ UseContext.js
@@ -0,0 +1,8 @@
+const Data = [
+  { flower: "Rose", colour: "pink" },
+  { flower: "Jasmine", colour: "white" },
+  { flower: "Sunflower", colour: "yellow" },
+  { flower: "Hibiscus", colour: "red" },
+];
+
+export default Data;
