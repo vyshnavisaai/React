c8384e0f9f2332fa65f447d14116e1c8fc891dce
vyshnavisaai <vyshnavihrishikeshpv@gmail.com>
2023-11-03 21:39:15 +0530

    Task :useContext,useNavigate,useState (display)

---
+++ Task3.jsx
@@ -0,0 +1,38 @@
+import React, { useContext } from 'react'
+import { newcontexttask } from './TaskRouting';
+import './Task.css'
+import { Button, Image } from 'react-bootstrap';
+import { useNavigate } from 'react-router-dom';
+
+function Task3() {
+    const [Fruit,setFruit] = useContext(newcontexttask)
+    console.log(Fruit);
+    const nav=useNavigate()
+    const back=()=>{
+        nav(-1)
+    }
+  return (
+    <div id='body'>
+      <center>
+        <div id='section1'>
+          
+        {Fruit.map((map1)=>{
+            return(
+            <>
+            <hr />
+            <h1>{map1.fruit}</h1>
+            <Image src={map1.Image} width={350} height={200} />
+            <p>{map1.def}</p>
+            <hr style={{color:'red'}}/>
+            </>
+        )
+        }
+        )}
+        </div>
+        <Button id='button' onClick={back}>Back</Button>
+        </center>
+    </div>
+  )
+}
+
+export default Task3
