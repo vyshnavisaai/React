cf0f4b90f2a18f84ec2f68352eeda8528406db39
vyshnavisaai <vyshnavihrishikeshpv@gmail.com>
2023-11-04 12:46:13 +0530

    Task :useContext,useNavigate,useState (js)

--- Task1.js
+++ Task1.js
@@ -1,22 +1,31 @@
 const fruits = [
   {
+    id:1,
     fruit: "Apple",
     Image:
       "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRyCoVvFRMWu7ZUgzjpXMoldZgfY0_fqKspQA&usqp=CAU",
     def: "An apple is a round, edible fruit produced by an apple tree. Apple trees are cultivated worldwide and are the most widely grown species.",
   },
   {
+    id:2,
     fruit: "Mango",
     Image:
       "https://www.verywellhealth.com/thmb/hXMvqirIZ98Gn6w5bkhO4UkkXRE=/1500x0/filters:no_upscale():max_bytes(150000):strip_icc()/GettyImages-1392614128-b6ff76b34a7f429995ed571015d1a680.jpg",
     def: "A mango is an edible stone fruit produced by the tropical tree Mangifera indica. It is believed to have originated in southern Asia.",
   },
   {
+    id:3,
     fruit: "Pomagranate",
     Image:
       "https://images.healthshots.com/healthshots/en/uploads/2021/09/27184641/pomegranate-1600x900.jpg",
     def: "The pomegranate (Punica granatum) is a fruit-bearing deciduous shrub in the family Lythraceae, subfamily Punicoideae.",
   },
+  {
+    id:4,
+    fruit:'Grapes',
+    Image:'https://www.cupitfood.com/wp-content/uploads/2018/08/grapes-1920x960.jpg',
+    def:'A grape is a fruit, botanically a berry, of the deciduous woody vines of the flowering plant genus Vitis. Grapes are a non-climacteric type of fruit, generally occurring in clusters.'
+  }
 ];
 
 export default fruits;
